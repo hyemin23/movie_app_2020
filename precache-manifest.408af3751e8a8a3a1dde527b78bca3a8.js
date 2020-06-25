be8b03bcc3fb429bbaf3ed945f632fdfaeffe0b5
hyemin23 <hm_stom@naver.com>
2020-06-26 01:00:23 +0900

    Updates

---
+++ precache-manifest.408af3751e8a8a3a1dde527b78bca3a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288b9b20b7bf0383bb51e631bc520256",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "cb433ce26a5d02c8735e",
+    "url": "/movie_app_2020/static/css/main.bceabe78.chunk.css"
+  },
+  {
+    "revision": "33780f4b91740a385a8a",
+    "url": "/movie_app_2020/static/js/2.dd1bf6f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.dd1bf6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb433ce26a5d02c8735e",
+    "url": "/movie_app_2020/static/js/main.7a4f0adc.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
